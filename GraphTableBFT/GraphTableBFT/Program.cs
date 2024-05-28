50700588a6af00cfe2009c94a51446a604263d08
sasha <sasha.savenkov.2002@mail.ru>
2024-05-29 00:59:12 +0400

    Ready

---
+++ GraphTableBFT/GraphTableBFT/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Graph
+{
+    private int[,] adjacencyMatrix;
+    private int vertices;
+
+    public Graph(int vertexCount)
+    {
+        vertices = vertexCount;
+        adjacencyMatrix = new int[vertices, vertices];
+    }
+
+    public void AddEdge(int source, int destination)
+    {
+        adjacencyMatrix[source, destination] = 1;
+        adjacencyMatrix[destination, source] = 1;
+    }
+
+    public void BFS(int start)
+    {
+        bool[] visited = new bool[vertices];
+        Queue<int> queue = new Queue<int>();
+
+        visited[start] = true;
+        queue.Enqueue(start);
+
+        Console.WriteLine("Обход в ширину:");
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+            Console.Write(current + " ");
+
+            for (int i = 0; i < vertices; i++)
+            {
+                if (adjacencyMatrix[current, i] == 1 && !visited[i])
+                {
+                    visited[i] = true;
+                    queue.Enqueue(i);
+                }
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        Graph graph = new Graph(7);
+
+        graph.AddEdge(0, 1);
+        graph.AddEdge(0, 2);
+        graph.AddEdge(1, 3);
+        graph.AddEdge(1, 4);
+        graph.AddEdge(2, 5);
+        graph.AddEdge(2, 6);
+
+        graph.BFS(0);
+    }
+}
